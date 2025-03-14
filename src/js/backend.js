d157a478c076fe4830b1dcb59c64594ee8c99f1a
Veloza Juan <juan.veloza_arcila@edu.univ-fcomte.fr>
2025-03-14 09:53:06 +0100

    commit test

--- src/js/backend.js
+++ src/js/backend.js
@@ -1,5 +1,6 @@
 import PocketBase from 'pocketbase';
-const POCKETBASE_URL = 'http://127.0.0.1:8090';
+// const POCKETBASE_URL = 'http://127.0.0.1:8090';
+const POCKETBASE_URL = 'https://festivalcinema.camiloveloza.fr';
 
 const pb =new PocketBase (POCKETBASE_URL);
 
